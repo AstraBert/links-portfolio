320ed3b6e3bdcc7c4d74b08ae3a7a0bc08698189
Clelia (Astra) Bertelli <clelia@runllama.ai>
2026-09-08 22:07:38 +0200

    feat: migrate to static deployment on GH pages

--- next.config.js
+++ next.config.js
@@ -2,11 +2,14 @@
 const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
+    unoptimized: true
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: 'standalone',
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH || '',
+  assetPrefix: process.env.PAGES_BASE_PATH || '',
 }
 
 module.exports = nextConfig
